327735a7fe912cbacbdb5c82b4cd7d319d436a94
Asb74 <amtoniosazon@gmail.com>
2025-08-04 18:10:29 +0200

    fix: add actual implementation of podhelper.rb

--- ios/Flutter/podhelper.rb
+++ ios/Flutter/podhelper.rb
@@ -1,1 +1,25 @@
-require File.expand_path(File.join('packages', 'flutter_tools', 'bin', 'podhelper'), File.dirname(File.realpath(__FILE__)))
+def install_all_flutter_pods(flutter_application_path)
+  install_flutter_engine_pod
+  install_flutter_plugin_pods(flutter_application_path)
+end
+
+def install_flutter_engine_pod
+  engine_dir = File.join(__dir__, 'engine')
+  if !File.exist?(engine_dir)
+    engine_dir = File.join(File.dirname(`flutter --version`), 'bin', 'cache', 'artifacts', 'engine', 'ios')
+  end
+
+  pod 'Flutter', :path => engine_dir
+end
+
+def install_flutter_plugin_pods(flutter_application_path)
+  plugin_manifest_path = File.join(flutter_application_path, '.flutter-plugins-dependencies')
+  return unless File.exist?(plugin_manifest_path)
+
+  plugin_manifest = JSON.parse(File.read(plugin_manifest_path))
+  plugin_manifest['plugins']['ios'].each do |plugin|
+    plugin_name = plugin['name']
+    plugin_path = plugin['path']
+    pod plugin_name, :path => File.expand_path(plugin_path, flutter_application_path)
+  end
+end
